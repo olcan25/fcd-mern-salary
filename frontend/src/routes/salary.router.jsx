44b54c42b3e4218c2f9ae994e6ebd6e46614c3ee
Olcan Pinduk Xeon <olcan.pinduk@outlook.com>
2022-05-28 08:36:00 +0200

    salary baslaym

---
+++ frontend/src/routes/salary.router.jsx
@@ -0,0 +1,9 @@
+import { lazy } from "react";
+
+// const SalaryCreate = lazy(() => import("../views"));
+
+// const salaryRoutes = [
+//     { path: "/salaries/new", element: <SalaryCreate /> },
+// ];
+
+export default salaryRoutes;
